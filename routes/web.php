a9f7962b64428f49d8378b40763e0add40fcdab9
BlutzerZ <wafiqnurudin@gmail.com>
2024-08-31 18:03:11 +0700

    Create Role Management, Closes #2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserManagementController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,8 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('auth.showLogin');
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/dashboard/user-management', [UserManagementController::class, 'show'])->name('userManagement.show');
+    Route::post('/dashboard/user-management/edit', [UserManagementController::class, 'edit'])->name('userManagement.edit');
+});
