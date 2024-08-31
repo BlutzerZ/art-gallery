c25be38ec48c5c36bbf5e3099ab56bc5298b8f2f
Wibowo Mulyo <111202214787@mhs.dinus.ac.id>
2024-09-01 04:38:02 +0700

    Add UI for user management

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", "Roboto", "sans-serif"],
+            },
+            colors: {
+                danger: "#9E3F39",
+                youngdanger: "#CB8E74",
+                light: "#EFECE2",
+                primary: "#003676",
+                warning: "#E2BD5F",
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
